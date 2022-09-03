f6e264be4a1b4fd956ed04ef7859f95d9064bf54
Riku Ueda <riku.ueda@outlook.jp>
2022-09-04 07:10:33 +0900

    バグ修正とデバッグ情報

--- src/main/java/tech/inudev/changeskin/CommandClass.java
+++ src/main/java/tech/inudev/changeskin/CommandClass.java
@@ -11,8 +11,10 @@ public class CommandClass implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (command.getName().equalsIgnoreCase("reloadskin")) {
             Bukkit.getServer().getOnlinePlayers().forEach(e ->
-                    ChangeSkin.getTeamData().forEach((t) ->
-                            ChangeSkin.getInstance().ChangePlayerSkin(e, t.get("team"), t.get("base"))));
+                    ChangeSkin.getTeamData().forEach((t) -> {
+                        ChangeSkin.getInstance().ChangePlayerSkin(e, t.get("team"), t.get("skin"));
+                        ChangeSkin.getInstance().getLogger().info(t.get("team") + " " + t.get("skin"));
+                    }));
             return true;
         }
         return false;
